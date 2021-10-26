4328cb5a32a068b0eec56d8ec20b153caa749181
Marcus Ricketts <marcusricketts@Marcuss-MacBook-Pro.local>
2021-10-25 21:50:12 -0400

    Installed nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 const connectDb = require("./startup/db");
-const connectDB = require("./startup/db");
+const express = require("express");
+const app = express();
 
 connectDb();
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
