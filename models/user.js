f819dd5cdfaa13ef3c9f25f07ff07cfb97287778
Marcus Ricketts <marcusricketts@Marcuss-MacBook-Pro.local>
2021-10-28 00:55:59 -0400

    created user schema file in models folder

--- models/user.js
+++ models/user.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const { dishSchema } = require("./dish");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  isAdministrator: { type: Boolean, default: false },
+});
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+  });
+  return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
