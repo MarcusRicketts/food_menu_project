9941c20e0b3e1ee078c1f7b322f07e9d34a2677b
Marcus Ricketts <marcusricketts@Marcuss-MacBook-Pro.local>
2021-10-28 21:55:01 -0400

    added user schema file to models folder

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const { dishSchema } = require("./dish");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
